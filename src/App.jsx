2b14444958c914168268c525c662ae78a4c7eace
AayushBhusari <bhusariaayush29@gmail.com>
2024-05-15 13:20:36 +0530

    Added Loading Animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,33 @@
 import Home from "./Components/Home";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import TopRated from "./Components/TopRated";
 import Upcoming from "./Components/Upcoming";
-import Navbar from "./Components/Navbar";
 import NotFound from "./Components/NotFound";
 import Movie from "./Components/Movie";
+import { SwitchTransition, CSSTransition } from "react-transition-group";
+import "./App.css";
 
 function App() {
+  const location = useLocation();
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Navbar />}>
-          {" "}
-          <Route path="/" element={<Home />} />
-          <Route path="/top-rated" element={<TopRated />} />
-          <Route path="/upcoming" element={<Upcoming />} />
-          <Route path="/movie/:movieID" element={<Movie />} />
-        </Route>
-        <Route path="/*" element={<NotFound />} />
-      </Routes>
+      <SwitchTransition component={null}>
+        <CSSTransition
+          key={location.pathname}
+          classNames="fade"
+          timeout={300}
+          unmountOnExit
+        >
+          <Routes location={location}>
+            {" "}
+            <Route path="/" element={<Home />} />
+            <Route path="/top-rated" element={<TopRated />} />
+            <Route path="/upcoming" element={<Upcoming />} />
+            <Route path="/movie/:movieID" element={<Movie />} />
+            <Route path="/*" element={<NotFound />} />
+          </Routes>
+        </CSSTransition>
+      </SwitchTransition>
     </>
   );
 }
